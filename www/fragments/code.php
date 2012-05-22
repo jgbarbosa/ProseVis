8c5686f545de3f9418b8aa9ee01db2a572666e1e
wiley <wiley@tclement.(none)>
2012-05-21 22:53:34 -0500

    Add download links

--- www/fragments/code.php
+++ www/fragments/code.php
@@ -3,5 +3,17 @@ ProseVis is available for Windows, Linux, and OSX.  Please click the appropriate
 </p></div>
 
 <div><p>
+<a href="<?php echo $site_prefix; ?>/downloads/prosevis.win64.zip" title="Windows 64 Bit Package">Windows 64 bit package</a> <br />
+<a href="<?php echo $site_prefix; ?>/downloads/prosevis.win32.zip" title="Windows 32 Bit Package">Windows 32 bit package</a> <br />
+<a href="<?php echo $site_prefix; ?>/downloads/prosevis.linux64.tar.gz" title="Linux 64 Bit Package">Linux 64 bit package</a> <br />
+<a href="<?php echo $site_prefix; ?>/downloads/prosevis.linux32.tar.gz" title="Linux 32 Bit Package">Linux 32 bit package</a> <br />
+<a href="<?php echo $site_prefix; ?>/downloads/prosevis.mac.zip" title="Mac OSX Package">Mac OSX package</a> <br />
+
+Below is a set of example data.
+
+<a href="<?php echo $site_prefix; ?>/downloads/data.zip" title="Sample Documents">Sample datasets</a>
+</p></div>
+
+<div><p>
 ProseVis is an open source project hosted on GitHub.  You can download the source or fork the project at <a href="https://github.com/christopherwiley/ProseVis" title="ProseVis Git Repository">https://github.com/christopherwiley/ProseVis</a>
 </p></div>
