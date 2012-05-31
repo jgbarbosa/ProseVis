40255066150f6ab38d33814a481a1d5c4865cdc1
Chris Wiley <wiley@wiley.im>
2012-05-31 03:23:42 +0000

    Fix for gather_packages

--- scripts/gather_packages.sh
+++ scripts/gather_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd executables
-zip all_packages.zip prosevis.linux32.tar.gz \
+tar czvf all_packages.tgz prosevis.linux32.tar.gz \
   prosevis.linux64.tar.gz \
   prosevis.mac.tar.gz \
   prosevis.win32.zip \
